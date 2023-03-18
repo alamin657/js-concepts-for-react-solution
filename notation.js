76a9c3b831bfacfad71addda7344a304fb3f18c3
Alamin <alamin017514@gmail.com>
2023-03-18 12:21:04 -0500

    added notation(dot notation and bracket notation

---
+++ notation.js
@@ -0,0 +1,17 @@
+const person = {
+    name: 'Lal Nil Holud mia',
+    profession: 'Traffic Surgeon',
+    age: 39,
+    25: 'Summer',
+    'son-name': 'Sunny',
+    address: 'Shahabag'
+}
+// dot notation
+const prof1 = person.profession;
+// console.log(prof1);
+// bracket notation 
+const prof2 = person['age'];
+// console.log(prof2);
+const prof = 'address';
+const prof3 = person[prof];
+// console.log(prof3);
